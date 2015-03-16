31372fcbcab881b3650bab6cc51e6b4ac2af5f46
ceilfors <wisen@ceilfors.com>
2015-03-16 19:10:09 +0000

    Made package-info.java compilable with JDK8.

--- src/main/java/org/apache/maven/plugins/enforcer/package-info.java
+++ src/main/java/org/apache/maven/plugins/enforcer/package-info.java
@@ -1,6 +1,6 @@
 /**
- * Contains all of the custom rules made available by this module.<br />
- * <br />
+ * Contains all of the custom rules made available by this module.<br>
+ * <br>
  * Package naming is following the maven enforcer plugin for user convenience i.e. doesn't need to
  * explicitly specify the implementation attribute. For more details on how maven map a configuration to
  * objects see the <a href="http://maven.apache.org/guides/mini/guide-configuring-plugins.html#Mapping_Complex_Objects">
